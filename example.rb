bfbea196070684e5d8b17476eb0aeee2e49b36ff
David Litvak <david.litvakb@gmail.com>
2012-01-01 11:00:56 -0300

    moved example to example.rb

---
+++ example.rb
@@ -0,0 +1,19 @@
+require 'deklarativna'
+require 'deklarativna_utils'
+
+include Deklarativna
+
+renderable = html {[
+               head {
+                 style {
+                   "p { font-size: 38px; color: red; }"
+                 }
+               },
+               body {
+                 paragraphs = []
+                 ["john", "dani", "david"].each { |e| paragraphs.push(p { "hola #{e}" }) }
+                 paragraphs
+               }
+             ]}
+
+HTMLExporter.new.launch_rendered_html_on_browser renderable
