b087062035d0d4600f93a121960a7874491b5b2c
David Litvak <david.litvakb@gmail.com>
2012-01-01 14:00:02 -0300

    removing them from dsl file

--- deklarativna.rb
+++ deklarativna.rb
@@ -2,21 +2,6 @@ require 'deklarativna_core'
 
 module Deklarativna
 
-  def renderable_string renderable_class, tag_name, block
-    (renderable_class.new { |instance|
-      instance.tag_name = tag_name
-      instance.content = block
-    }).to_s
-  end
-
-  def nesting_renderable_string tag_name, block
-    renderable_string NestingRenderable, tag_name, block
-  end
-
-  def text_renderable_string tag_name, block
-    renderable_string TextRenderable, tag_name, block
-  end
-
   def html &html_block
     nesting_renderable_string "html", html_block
   end
