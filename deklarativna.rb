7838cd8d4f33eaaf7cd870862fdf9ef14210fdf8
David Litvak <david.litvakb@gmail.com>
2012-01-02 18:56:01 -0300

    renamed extra_tags to attributes in dsl

--- deklarativna.rb
+++ deklarativna.rb
@@ -2,200 +2,200 @@ require 'deklarativna_core'
 
 module Deklarativna
 
-  def html extra_tags={}, &html_block
-    nesting_renderable_string "html", html_block, extra_tags
+  def html attributes={}, &html_block
+    nesting_renderable_string "html", html_block, attributes
   end
 
-  def head extra_tags={}, &html_block
-    nesting_renderable_string "head", html_block, extra_tags
+  def head attributes={}, &html_block
+    nesting_renderable_string "head", html_block, attributes
   end
 
   def title &page_title_block
     text_renderable_string "title", page_title_block
   end
 
-  def link rel="", href="", extra_tags={}
-    extra_tags["href"] = href
-    extra_tags["rel"] = rel
-    single_tag_renderable_string "link", extra_tags
+  def link rel="", href="", attributes={}
+    attributes["href"] = href
+    attributes["rel"] = rel
+    single_tag_renderable_string "link", attributes
   end
 
-  def meta tags={}
-    single_tag_renderable_string "meta", tags
+  def meta attributes={}
+    single_tag_renderable_string "meta", attributes
   end
 
-  def script type="", extra_tags={}, &script_text_block
-    extra_tags["type"] = type
-    text_renderable_string "script", script_text_block, extra_tags
+  def script type="", attributes={}, &script_text_block
+    attributes["type"] = type
+    text_renderable_string "script", script_text_block, attributes
   end
 
-  def javascript extra_tags={}, &script_text_block
-    script "text/javascript", extra_tags, &script_text_block
+  def javascript attributes={}, &script_text_block
+    script "text/javascript", attributes, &script_text_block
   end
 
-  def style type="", extra_tags={}, &style_text_block
-    extra_tags["type"] = type
-    text_renderable_string "style", style_text_block, extra_tags
+  def style type="", attributes={}, &style_text_block
+    attributes["type"] = type
+    text_renderable_string "style", style_text_block, attributes
   end
 
-  def css extra_tags={}, &style_text_block
-    style "text/css", extra_tags, &style_text_block
+  def css attributes={}, &style_text_block
+    style "text/css", attributes, &style_text_block
   end
 
-  def body extra_tags={}, &html_block
-    nesting_renderable_string "body", html_block, extra_tags
+  def body attributes={}, &html_block
+    nesting_renderable_string "body", html_block, attributes
   end
 
-  def h1 extra_tags={}, &html_block
-    text_renderable_string "h1", html_block, extra_tags
+  def h1 attributes={}, &html_block
+    text_renderable_string "h1", html_block, attributes
   end
 
-  def h2 extra_tags={}, &html_block
-    text_renderable_string "h2", html_block, extra_tags
+  def h2 attributes={}, &html_block
+    text_renderable_string "h2", html_block, attributes
   end
 
-  def h3 extra_tags={}, &html_block
-    text_renderable_string "h3", html_block, extra_tags
+  def h3 attributes={}, &html_block
+    text_renderable_string "h3", html_block, attributes
   end
 
-  def h4 extra_tags={}, &html_block
-    text_renderable_string "h4", html_block, extra_tags
+  def h4 attributes={}, &html_block
+    text_renderable_string "h4", html_block, attributes
   end
 
-  def h5 extra_tags={}, &html_block
-    text_renderable_string "h5", html_block, extra_tags
+  def h5 attributes={}, &html_block
+    text_renderable_string "h5", html_block, attributes
   end
 
-  def h6 extra_tags={}, &html_block
-    text_renderable_string "h6", html_block, extra_tags
+  def h6 attributes={}, &html_block
+    text_renderable_string "h6", html_block, attributes
   end
 
-  def p extra_tags={}, &html_block
-    nesting_renderable_string "p", html_block, extra_tags
+  def p attributes={}, &html_block
+    nesting_renderable_string "p", html_block, attributes
   end
 
-  def div extra_tags={}, &html_block
-    nesting_renderable_string "div", html_block, extra_tags
+  def div attributes={}, &html_block
+    nesting_renderable_string "div", html_block, attributes
   end
 
-  def span extra_tags={}, &html_block
-    nesting_renderable_string "span", html_block, extra_tags
+  def span attributes={}, &html_block
+    nesting_renderable_string "span", html_block, attributes
   end
 
-  def table extra_tags={}, &html_block
-    nesting_renderable_string "table", html_block, extra_tags
+  def table attributes={}, &html_block
+    nesting_renderable_string "table", html_block, attributes
   end
 
-  def tr extra_tags={}, &html_block
-    nesting_renderable_string "tr", html_block, extra_tags
+  def tr attributes={}, &html_block
+    nesting_renderable_string "tr", html_block, attributes
   end
 
-  def td extra_tags={}, &html_block
-    nesting_renderable_string "td", html_block, extra_tags
+  def td attributes={}, &html_block
+    nesting_renderable_string "td", html_block, attributes
   end
 
-  def ul extra_tags={}, &html_block
-    nesting_renderable_string "ul", html_block, extra_tags
+  def ul attributes={}, &html_block
+    nesting_renderable_string "ul", html_block, attributes
   end
 
-  def ol extra_tags={}, &html_block
-    nesting_renderable_string "ol", html_block, extra_tags
+  def ol attributes={}, &html_block
+    nesting_renderable_string "ol", html_block, attributes
   end
 
-  def li extra_tags={}, &html_block
-    nesting_renderable_string "li", html_block, extra_tags
+  def li attributes={}, &html_block
+    nesting_renderable_string "li", html_block, attributes
   end
 
-  def a href="", extra_tags={}, &html_block
-    extra_tags["href"] = href
-    nesting_renderable_string "a", html_block, extra_tags
+  def a href="", attributes={}, &html_block
+    attributes["href"] = href
+    nesting_renderable_string "a", html_block, attributes
   end
 
-  def form action="", method="", extra_tags={}, &html_block
-    extra_tags["action"] = action
-    extra_tags["method"] = method
-    nesting_renderable_string "form", html_block, extra_tags
+  def form action="", method="", attributes={}, &html_block
+    attributes["action"] = action
+    attributes["method"] = method
+    nesting_renderable_string "form", html_block, attributes
   end
 
-  def text_input name="", extra_tags={}
-    form_input_renderable_string name, "text", extra_tags
+  def text_input name="", attributes={}
+    form_input_renderable_string name, "text", attributes
   end
 
-  def password_input name="", extra_tags={}
-    form_input_renderable_string name, "password", extra_tags
+  def password_input name="", attributes={}
+    form_input_renderable_string name, "password", attributes
   end
 
-  def radio_input name="", value="", extra_tags={}
-    extra_tags["value"] = value
-    form_input_renderable_string name, "radio", extra_tags
+  def radio_input name="", value="", attributes={}
+    attributes["value"] = value
+    form_input_renderable_string name, "radio", attributes
   end
 
-  def checkbox_input name="", value="", extra_tags={}
-    extra_tags["value"] = value
-    form_input_renderable_string name, "checkbox", extra_tags
+  def checkbox_input name="", value="", attributes={}
+    attributes["value"] = value
+    form_input_renderable_string name, "checkbox", attributes
   end
 
-  def submit value="Submit", extra_tags={}
-    extra_tags["type"] = "submit"
-    extra_tags["value"] = value
-    single_tag_renderable_string "input", extra_tags
+  def submit value="Submit", attributes={}
+    attributes["type"] = "submit"
+    attributes["value"] = value
+    single_tag_renderable_string "input", attributes
   end
 
-  def center extra_tags={}, &html_block
-    nesting_renderable_string "center", html_block, extra_tags
+  def center attributes={}, &html_block
+    nesting_renderable_string "center", html_block, attributes
   end
 
-  def dd extra_tags={}, &html_block
-    nesting_renderable_string "dd", html_block, extra_tags
+  def dd attributes={}, &html_block
+    nesting_renderable_string "dd", html_block, attributes
   end
 
-  def dl extra_tags={}, &html_block
-    nesting_renderable_string "dl", html_block, extra_tags
+  def dl attributes={}, &html_block
+    nesting_renderable_string "dl", html_block, attributes
   end
 
-  def dt extra_tags={}, &html_block
-    nesting_renderable_string "dt", html_block, extra_tags
+  def dt attributes={}, &html_block
+    nesting_renderable_string "dt", html_block, attributes
   end
 
-  def i extra_tags={}, &html_block
-    nesting_renderable_string "i", html_block, extra_tags
+  def i attributes={}, &html_block
+    nesting_renderable_string "i", html_block, attributes
   end
 
-  def b extra_tags={}, &html_block
-    nesting_renderable_string "b", html_block, extra_tags
+  def b attributes={}, &html_block
+    nesting_renderable_string "b", html_block, attributes
   end
 
-  def em extra_tags={}, &html_block
-    nesting_renderable_string "em", html_block, extra_tags
+  def em attributes={}, &html_block
+    nesting_renderable_string "em", html_block, attributes
   end
 
-  def strong extra_tags={}, &html_block
-    nesting_renderable_string "strong", html_block, extra_tags
+  def strong attributes={}, &html_block
+    nesting_renderable_string "strong", html_block, attributes
   end
 
-  def pre extra_tags={}, &preformatted_text_block
-    text_renderable_string "pre", preformatted_text_block, extra_tags
+  def pre attributes={}, &preformatted_text_block
+    text_renderable_string "pre", preformatted_text_block, attributes
   end
 
-  def br extra_tags={}
-    single_tag_renderable_string "br", extra_tags
+  def br attributes={}
+    single_tag_renderable_string "br", attributes
   end
 
   def comment &comment_block
     comment_renderable_string comment_block
   end
 
-  def img src="", alt="", extra_tags={}
-    extra_tags["src"] = src
-    extra_tags["alt"] = alt
-    single_tag_renderable_string "img", extra_tags
+  def img src="", alt="", attributes={}
+    attributes["src"] = src
+    attributes["alt"] = alt
+    single_tag_renderable_string "img", attributes
   end
 
-  def xml_single_tag tag_name, extra_tags={}
-    single_tag_renderable_string tag_name.downcase, extra_tags
+  def xml_single_tag tag_name, attributes={}
+    single_tag_renderable_string tag_name.downcase, attributes
   end
 
-  def xml_double_tag tag_name, extra_tags={}, &html_block
-    nesting_renderable_string tag_name.downcase, html_block, extra_tags
+  def xml_double_tag tag_name, attributes={}, &html_block
+    nesting_renderable_string tag_name.downcase, html_block, attributes
   end
 end
