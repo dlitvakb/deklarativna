1b8d55d9711842da2f6db5bc1e471d5a68835973
David Litvak <david.litvakb@gmail.com>
2012-01-03 00:18:12 -0300

    more interesting sinatra examples

--- static_sinatra_example.rb
+++ static_sinatra_example.rb
@@ -47,7 +47,18 @@ end
 
 class IndexTemplate < BaseTemplate
   def _content
-    p { "Testing Static File Generation using sinatra" }
+    [
+      p { "Testing Static File Generation using sinatra" },
+      table("border"=>"1") {[
+        tr {[
+          td { "row 1, value 1" },
+          td { "row 1, value 2" }
+        ]},
+        tr {[
+          td("colspan"=>"2") { "Only value in this row" }
+        ]}
+      ]}
+    ]
   end
 end
 
