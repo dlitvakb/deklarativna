0e41e939e8348789d281affae3493b3d1553c245
David Litvak <david.litvakb@gmail.com>
2012-01-03 21:03:58 -0300

    run_tests now use rake

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,1 @@
-#rvm use ruby-1.9.2
-#rvm -S _deklarativna_test.rb
-#rvm -S _deklarativna_core_test.rb
-
-#back to default ruby until i get to move to ruby-1.9.2 as my default ruby
-ruby _deklarativna_test.rb
-ruby _deklarativna_core_test.rb
+rake test
